504dfb044c749cddc6cd25942deac63e1f396130
chikaMiracle <chikamiracle28@gmail.com>
2020-06-26 16:21:30 +0100

    Initiate commit

---
+++ MainDatingApp/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using MainDatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainDatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
